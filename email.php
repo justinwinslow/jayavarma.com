887f1613f4b97d06161d32b9ad10190bc4bcbeff
Justin Winslow <justin@jwin.me>
2013-12-16 21:22:27 -0600

    Mailgun integration

--- email.php
+++ email.php
@@ -1,12 +1,25 @@
 <?php
+	# Include the Autoloader (see "Libraries" for install instructions)
+	require 'vendor/autoload.php';
+	use Mailgun\Mailgun;
+
+	# Instantiate the client.
+	$mgClient = new Mailgun('key-27u8fvlqultyil204j3aesrl1rkeoc10');
+	$domain = 'jayavarma.com';
+
 	$name = $_REQUEST['name'] ;
 	$email = $_REQUEST['email'] ;
 	$msg = $_REQUEST['message'] ;
 	//$validation_1 = $_REQUEST['form_contact_math'] ;
 	$validation_2 = $_REQUEST['subject'] ;
-	
+
 	if(empty($validation_2)){
-		mail( "jayakvarma@gmail.com", "JayaVarma.com Contact Form | From: $name", "$msg", "From: $email" );
+		# Make the call to the client.
+		$result = $mgClient->sendMessage("$domain",
+                  array('from'    => '$name <$email>',
+                        'to'      => 'jusrwin@gmail.com', #jayakvarma@gmail.com
+                        'subject' => 'JayaVarma.com Contact Form | From: $name',
+                        'text'    => '$msg'));
 	}else{
 		echo "I don't think so.";
 	}
